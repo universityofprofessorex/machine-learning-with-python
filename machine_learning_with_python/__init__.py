42eb6787f5a2e24ec76067f1379d21f257358bca
Malcolm Jones <bossjones@theblacktonystark.com>
2021-08-27 15:37:58 -0400

    ML: init

---
+++ machine_learning_with_python/__init__.py
@@ -0,0 +1,16 @@
+"""machine-learning-with-python
+
+Author: iamprofessorex
+Created:
+"""
+
+__version__ = "0.0.1"
+__short_description__ = "sentdex ml tutorial"
+__license__ = "Apache 2.0"
+__author__ = "@IamProfessorEX"
+__author_email__ = "jarvis@theblacktonystark.com"
+__maintainer__ = "@IamProfessorEX"
+__maintainer_email__ = "jarvis@theblacktonystark.com"
+__github_username__ = "IamProfessorEX"
+__copyright__ = "2020-2021"
+__repo_name__ = "https://github.com/universityofprofessorex/machine-learning-with-python"
